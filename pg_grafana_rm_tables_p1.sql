cfbeac5935351ae841a897771aef74b0141e2e47
Oracle Jedi <oracle_jedi@yahoo.com>
2023-11-22 18:13:29 -0800

    Create pg_grafana_rm_tables_p1.sql

---
+++ pg_grafana_rm_tables_p1.sql
@@ -0,0 +1,14 @@
+/* 
+
+drop the tables in Postgres to hold the data
+
+these are used read_ora_stats.ksh script
+
+*/
+
+drop table ORA_SESSIONS_NOTIME;
+
+drop table ORA_OSSTATS;
+
+drop table ORA_OSSTATS_DELTA;
+
