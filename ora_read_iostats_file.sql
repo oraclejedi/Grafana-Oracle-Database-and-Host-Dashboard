fe9da4a5bfce93f2eafe17786f5240dd63cb0074
Oracle Jedi <oracle_jedi@yahoo.com>
2023-11-28 10:08:55 -0800

    Create ora_read_iostats_file.sql

---
+++ ora_read_iostats_file.sql
@@ -0,0 +1,25 @@
+
+--
+-- reads the gv$iostat_file view and crafts a PostgreSQL insert statement
+--
+
+select
+  'insert into ORA_IOSTATS_FILE values('||
+  vdb.dbid||',now(),'||
+--  to_char(sysdate,'YYYYMMDDHH24MISS')||','||
+  sum(iof.small_read_reqs)||','||
+  sum(iof.small_write_reqs)||','||
+  sum(iof.large_read_reqs)||','||
+  sum(iof.large_write_reqs)||','||
+  sum(iof.small_read_megabytes)||','||
+  sum(iof.small_write_megabytes)||','||
+  sum(iof.large_read_megabytes)||','||
+  sum(iof.large_write_megabytes)||');' "-- postgres insert"
+from
+  v$database vdb,
+  gv$iostat_file iof
+where 1=1
+and iof.con_id=0
+group by vdb.dbid
+/
+
